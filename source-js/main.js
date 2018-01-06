ff642e4bd8b3b46036bd33a5600f7a9fe7ca1064
AnastasiaAndreevna <31144016+AnastasiaAndreevna@users.noreply.github.com>
2018-01-06 23:52:52 +0200

    amended commit

---
+++ source-js/main.js
@@ -0,0 +1,20 @@
+//popup
+jQuery(function () {
+  jQuery('#modal-trigger').click(function () {
+    jQuery('#modal_1').fadeIn(600)
+  })
+
+  jQuery(function () {
+    jQuery('#close').click(function () {
+      jQuery('#modal_1').fadeOut(300)
+    })
+  })
+})
+
+//sliders
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: 'slide',
+    controlNav: false,
+  });
+});
